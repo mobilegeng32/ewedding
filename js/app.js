a7ffd3d959e6a15700766a08bf2ebd91aa08afbe
mobilegeng32 <mobilegeng32@gmail.com>
2026-09-20 11:05:48 +0800

    test js loading

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzR8xQhsXVK-QhG17-E_8ffvvhTvYWcNyfqfU-xilsKxItcHA5Uwdr4WOq7jQ8Lj8XgJw/exec";
+document.getElementById("couple").innerText =
+    "Hazim & Utrias";
 
-fetch(API_URL)
-    .then(response => response.json())
-    .then(data => {
+document.getElementById("venue").innerText =
+    "Masjid Negeri Pulau Pinang";
 
-        document.getElementById("couple").innerText =
-            `${data.GroomName} & ${data.BrideName}`;
-
-        document.getElementById("venue").innerText =
-            `Venue: ${data.Venue}`;
-
-        document.getElementById("eventDate").innerText =
-            `Date: ${data.EventDate}`;
-
-        console.log(data);
-
-    })
-    .catch(error => {
-        console.error("API Error:", error);
-
-        document.getElementById("couple").innerText =
-            "Unable to load data";
-    });
+document.getElementById("eventDate").innerText =
+    "22 May 2027";
