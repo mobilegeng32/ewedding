bf31f4a677b3b700536a23465f8819d0680f2de5
mobilegeng32 <mobilegeng32@gmail.com>
2026-09-20 11:36:48 +0800

    connect google sheet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-document.getElementById("couple").innerText =
-    "Hazim & Utrias";
+const API_URL = "https://script.google.com/macros/s/AKfycbzR8xQhsXVK-QhG17-E_8ffvvhTvYWcNyfqfU-xilsKxItcHA5Uwdr4WOq7jQ8Lj8XgJw/exec";
 
-document.getElementById("venue").innerText =
-    "Masjid Negeri Pulau Pinang";
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
 
-document.getElementById("eventDate").innerText =
-    "22 May 2027";
+        document.getElementById("couple").innerText =
+            `${data.GroomName} & ${data.BrideName}`;
+
+        document.getElementById("venue").innerText =
+            data.Venue;
+
+        document.getElementById("eventDate").innerText =
+            data.EventDate;
+
+    })
+    .catch(error => {
+        console.error(error);
+    });
